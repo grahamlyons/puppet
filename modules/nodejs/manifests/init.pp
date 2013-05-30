aeceedded43f835de76b147e210eb59c6da0ae05
Graham Lyons <graham@grahamlyons.com>
2013-05-30 20:57:28 +0100

    Make sure that the package needed for adding ppas is installed.

--- modules/nodejs/manifests/init.pp
+++ modules/nodejs/manifests/init.pp
@@ -2,7 +2,13 @@ class nodejs {
 
     exec { 'add-node-repo':
         command => 'add-apt-repository ppa:chris-lea/node.js -y && apt-get update',
-        creates => '/etc/apt/sources.list.d/chris-lea-node_js-precise.list'
+        creates => '/etc/apt/sources.list.d/chris-lea-node_js-precise.list',
+        require => Package['python-software-properties'],
+        notify  => Exec['apt-get update']
+    }
+
+    package { 'python-software-properties':
+        ensure  => latest
     }
 
     package{ 'nodejs':
